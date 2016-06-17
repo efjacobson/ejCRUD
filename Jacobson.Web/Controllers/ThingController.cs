872183631ec46fa0958509e253d605e14ffe3355
Eric Jacobson <contact@efjacobson.com>
2016-06-17 11:36:34 -0700

    begin angular create form

---
+++ Jacobson.Web/Controllers/ThingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jacobson.Web.Controllers
+{
+    public class ThingController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Jacobson.Web/Scripts/Angular/thingCreateController.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('thingCreateApp')
+        .controller('thingCreateController', thingCreateController);
+
+    thingCreateController.$inject = ['$location']; 
+
+    function thingCreateController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'thingCreateController';
+
+        vm.submit = _submit;
+
+        activate();
+
+        function activate() { }
+
+        function _submit() {
+            console.log(vm.Name);
+            console.log(vm.Description);
+        }
+    }
+})();
